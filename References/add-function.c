4323e6ecd6e3c6ff7413884a0b7f7419ae656982
JellyFishJuggler <146509602+JellyFishJuggler@users.noreply.github.com>
2023-10-08 17:40:02 +0530

    Some changes

---
+++ References/add-function.c
@@ -0,0 +1,35 @@
+/* for reference this is how a new document is created.
+    'w' is used for creating a new file. aur isme joh data aayega voh last wale se replace hoga hmesha.
+    reason being ki yeh hr baar ek file create kr rha hai.
+    'a' is used for adding new datas in the already created file.
+    'w+' bhi use kr skte hai, yeh hota hai agr hme file ko read bhi krna ho.
+    (reading ka smjhna hai toh todha google krlena else maine reading func me dala hai tohda sa)
+
+    same tarike se 'a+' bhi hota hai. jisme hm new data to add karte ja rhe hai but usko phir read bhi
+    karpayenge end me ya jab mann kare
+
+    aur finally 'r' jo read karta hai. same tarike se 'r+' bhi hogya jisme read + writing hota hai. 
+
+    'r+' aur 'w+' me kya difference yeh google krlena mai nhi dekh rha ab. T-T
+    */
+#include <stdio.h>
+
+int main() {
+    // Open a file for writing (if it doesn't exist, it will be created)
+    FILE *file = fopen("example.txt", "w");
+
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;
+    }
+
+    // Write data to the file
+    fprintf(file, "Hello, this is a sample text.\n");
+    fprintf(file, "You can add more text here.\n");
+
+    // Close the file when done
+    fclose(file);
+
+    printf("File created and written successfully.\n");
+    return 0;
+}
---
+++ References/reading-function.c
@@ -0,0 +1,31 @@
+/*
+    reading ke baare me todha baht jaan lo
+    yadi koi file hoti hai hmare paas toh use load karke user ko show karane keliye read use hota hai
+    bht hee basic stuff haina....
+    bas yhi hai yeh
+
+    aur bhi kuch hote hai jaise rb,wb,ab aur unke '+' variations. inka abhi kaam nhi hai toh chill kro.
+*/
+#include <stdio.h>
+
+int main() {
+    // Open a file for reading
+    FILE *file = fopen("example.txt", "r");
+
+    if (file == NULL) {
+        printf("Error opening the file.\n");
+        return 1;
+    }
+
+    char buffer[100]; // Buffer to store read data
+
+    // Read and print data from the file
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        printf("%s", buffer);
+    }
+
+    // Close the file when done
+    fclose(file);
+
+    return 0;
+}
